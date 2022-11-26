fd3efbcd3f29da4e101ea397420803eeb05d8833
Guaderic <VladymyrGolovan@gmail.com>
2022-11-26 15:52:19 +0200

    add header

---
+++ src/Sidebar.js
@@ -0,0 +1,21 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className='sidebar_top'>
+        <img src="" alt=""/>
+        <Avatar className='sidebar_avatar'/>
+        <h2>Hello</h2>
+        <h3>gmail.com</h3>
+      </div>
+      <div className='sidebar_stats'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
