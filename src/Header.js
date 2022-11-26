f54423b22ee4d8dec1b1876dbf70debca1181297
Guaderic <VladymyrGolovan@gmail.com>
2022-11-26 15:52:48 +0200

    add header

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HeaderOption from './HeaderOption'
+
+
+function Header () {
+  return (
+    <div className='header'>
+
+      <div className='header__left'>
+        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt=""/>
+
+        <div className='header__search'>
+            <SearchIcon/>
+            <input type="text"/>
+        </div>
+      </div> 
+
+      <div className='header__right'>
+        <HeaderOption Icon ={HomeIcon} title='Home'/>  
+        <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/> 
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/> 
+        <HeaderOption Icon={ChatIcon} title='Messaging'/>
+        <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+        <HeaderOption avatar='https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png' title='me'/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
